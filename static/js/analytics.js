6450007c110a1943d14a13acd3057d28768030ee
Joel Weinberger <joel@greenpangia.com>
2011-08-14 15:01:00 -0700

    Fixed analytics JS to pass jslint

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,65 +1,70 @@
 var ANALYTICS = (function () {
-    var genChartData = function(data, dataPoint) {
-        var result = new Array();
-        var i;
+    'use strict';
+    var currentcharttype,
+        curplot,
+        data = [ 3, 8, 5, 13, 8, 2, 2, 9, 10, 1, 2, 5 ],
+        paused = false,
+        pausebtn,
+        timeout;
 
-        for (i = 0; i < data.length; i++) {
+    function genChartData(data, dataPoint) {
+        var result = [], i;
+
+        for (i = 0; i < data.length; i = i + 1) {
             result.push(dataPoint(data, i));
         }
 
-        return(result);
+        return (result);
     }
 
-    var overTimeData = function(data, index) {
-        return([ index, data[index] ]);
-    };
+    function overTimeData(data, index) {
+        return ([ index, data[index] ]);
+    }
 
-    var pieData = function(data, index) {
-        return({ data: data[index] });
-    };
+    function pieData(data, index) {
+        return ({ data: data[index] });
+    }
 
-    var data = [ 3, 8, 5, 13, 8, 2, 2, 9, 10, 1, 2, 5 ];
-
-    var currentcharttype;
-    var curplot;
-
-    function setAreaData () {
-        return([
+    function setAreaData() {
+        return ([
             {
                 lines: {
                     show: true,
-                    fill: true,
+                    fill: true
                 },
-                data: genChartData(data, overTimeData),
+                data: genChartData(data, overTimeData)
             }
         ]);
     }
 
-    function setBarData () {
-        return([
+    function setBarData() {
+        return ([
             {
                 bars: {
-                    show: true,
+                    show: true
                 },
-                data: genChartData(data, overTimeData),
+                data: genChartData(data, overTimeData)
             }
         ]);
     }
 
-    function setPieData () {
-        return(genChartData(data, pieData));
+    function setPieData() {
+        return (genChartData(data, pieData));
     }
 
     function setData(type) {
         var data;
 
         switch (type) {
-        case 'area': data = setAreaData();
-                     break;
-        case 'bar': data = setBarData();
-                    break;
-        case 'pie': data = setPieData();
-                    break;
+        case 'area':
+            data = setAreaData();
+            break;
+        case 'bar':
+            data = setBarData();
+            break;
+        case 'pie':
+            data = setPieData();
+            break;
         }
 
         return data;
@@ -69,71 +74,74 @@ var ANALYTICS = (function () {
         var options;
 
         switch (type) {
-        case 'area': options = {
-                        selection: {
-                            mode: 'x',
-                        },
-                     };
-                     break;
-        case 'bar': options = {};
-                    break;
-        case 'pie': options = {
-                        series: {
-                            pie: {
-                                show: true,
-                            },
-                        },
-                        grid: {
-                            hoverable: true,
-                            clickable: true,
-                        },
-                    };
-                    break;
+        case 'area':
+            options = {
+                selection: {
+                    mode: 'x'
+                }
+            };
+            break;
+        case 'bar':
+            options = {};
+            break;
+        case 'pie':
+            options = {
+                series: {
+                    pie: {
+                        show: true
+                    }
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: true
+                }
+            };
+            break;
         }
 
         return options;
     }
 
-    function areaSelected(event, ranges)
-    {
+    function areaSelected(event, ranges) {
         alert(ranges.xaxis.from + " : " + ranges.xaxis.to);
     }
 
-    function pieHover(event, pos, obj)
-    {
+    function pieHover(event, pos, obj) {
         /*if (!obj)
                 return;
         percent = parseFloat(obj.series.percent).toFixed(2);
         $("#hover").html('<span style="font-weight: bold; color: '+obj.series.color+'">'+obj.series.label+' ('+percent+'%)</span>');*/
     }
 
-    function pieClick(event, pos, obj)
-    {
-            alert("click");
-            /*if (!obj)
-                return;
-            percent = parseFloat(obj.series.percent).toFixed(2);
-            alert(''+obj.series.label+': '+percent+'%');*/
+    function pieClick(event, pos, obj) {
+        alert("click");
+        /*if (!obj)
+            return;
+        percent = parseFloat(obj.series.percent).toFixed(2);
+        alert(''+obj.series.label+': '+percent+'%');*/
     }
 
     function registerChartCallbacks(type, visualizationelement) {
         switch (type) {
-        case 'area': visualizationelement.bind("plotselected", areaSelected);
-                     break;
-        case 'bar': break;
-        case 'pie': visualizationelement.bind("plothover", pieHover);
-                    visualizationelement.bind("plotclick", pieClick);
-                    break;
+        case 'area':
+            visualizationelement.bind("plotselected", areaSelected);
+            break;
+        case 'bar':
+            break;
+        case 'pie':
+            visualizationelement.bind("plothover", pieHover);
+            visualizationelement.bind("plotclick", pieClick);
+            break;
         }
     }
 
     function dashboardChartTypeUpdate(chart, type) {
-        var data = setData(type);
-        var options = setOptions(type);
+        var data = setData(type),
+            options = setOptions(type),
+            visualizationelement = $('#flotvisualization');
 
         currentcharttype = type;
-        var visualizationelement = $('#flotvisualization');
-        curplot = $.plot($(visualizationelement), data, options);
+        curplot = $.plot(visualizationelement, data, options);
 
         registerChartCallbacks(type, visualizationelement);
     }
@@ -141,28 +149,28 @@ var ANALYTICS = (function () {
     currentcharttype = 'area';
     dashboardChartTypeUpdate(0, currentcharttype);
 
-    var timeout;
-    var timeoutCallback = function () {
+    function timeoutCallback() {
         var randomnumber = Math.floor(Math.random() * 13);
         data.shift();
         data.push(randomnumber);
         curplot.setData(setData(currentcharttype));
         curplot.draw();
         timeout = setTimeout(timeoutCallback, 2000);
-    };
+    }
+
     timeout = setTimeout(timeoutCallback, 2000);
-    var paused = false;
 
 
-    var genChartUpdate = function (type) {
+    function genChartUpdate(type) {
         return function () { dashboardChartTypeUpdate(0, type); };
-    };
+    }
+
     $('#btn_area').click(genChartUpdate('area'));
     $('#btn_bar').click(genChartUpdate('bar'));
     $('#btn_pie').click(genChartUpdate('pie'));
 
-    var pausebtn = $('#btn_pause');
-    pausebtn.click(function(obj) {
+    pausebtn = $('#btn_pause');
+    pausebtn.click(function (obj) {
         if (!paused) {
             clearTimeout(timeout);
             pausebtn.addClass('btn_pause');

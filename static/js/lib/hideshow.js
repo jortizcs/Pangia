a77a5ac1d2d12499257608e782cdc55457e402a1
Joel Weinberger <joel@greenpangia.com>
2011-10-18 21:58:05 -0700

    improved fade in/out order in hideshow

--- static/js/lib/hideshow.js
+++ static/js/lib/hideshow.js
@@ -2,59 +2,63 @@
 
 // this tells jquery to run the function below once the DOM is ready
 $(document).ready(function() {
-// choose text for the show/hide link - can contain HTML (e.g. an image)
-var showText='Show';
-var hideText='Hide';
+    // choose text for the show/hide link - can contain HTML (e.g. an image)
+    var showText='Show';
+    var hideText='Hide';
 
-// initialise the visibility check
-var is_visible = false;
+    // initialise the visibility check
+    var is_visible = false;
 
-// append show/hide links to the element directly preceding the element with a class of "toggle"
-$('.toggle').prev().append(' <a href="#" class="toggleLink">'+showText+'</a>');
-$('.toggle_sub').prev().append(' <a href="#" class="toggleLink_sub">'+showText+'</a>');
+    // append show/hide links to the element directly preceding the element with a class of "toggle"
+    $('.toggle').prev().append(' <a href="#" class="toggleLink">'+showText+'</a>');
+    $('.toggle_sub').prev().append(' <a href="#" class="toggleLink_sub">'+showText+'</a>');
 
-// hide all of the elements with a class of 'toggle'
-$('.toggle').hide();
-$('.toggle_sub').hide();
+    // hide all of the elements with a class of 'toggle'
+    $('.toggle').hide();
+    $('.toggle_sub').hide();
 
-// capture clicks on the toggle links
-$('a.toggleLink').click(function() {
+    // capture clicks on the toggle links
+    $('a.toggleLink').click(function() {
 
-// switch visibility
-is_visible = !is_visible;
+        // switch visibility
+        is_visible = !is_visible;
 
-// change the link text depending on whether the element is shown or hidden
-if ($(this).text()==showText) {
-$(this).text(hideText);
-$(this).parent().next('.toggle').slideDown('slow');
-    $("#building").fadeIn(350, 'swing');
-    $("#alert").fadeOut(350, 'swing');
-}
-else {
-$(this).text(showText);
-$(this).parent().next('.toggle').slideUp('slow');
-    $("#floor").fadeOut(450, 'swing');
-    $("#building").fadeOut(450, 'swing');
-    $("#alert").fadeIn(450, 'swing');
-}
-// return false so any link destination is not followed
-return false;
-});
+        // change the link text depending on whether the element is shown or hidden
+        if ($(this).text()==showText) {
+            $(this).text(hideText);
+            $(this).parent().next('.toggle').slideDown('slow');
+                $("#alert").fadeOut(350, 'swing',
+                    function () {
+                        $("#building").fadeIn(350, 'swing');
+                    });
+        }
+        else {
+            $(this).text(showText);
+            $(this).parent().next('.toggle').slideUp('slow');
+                $("#floor").fadeOut(450, 'swing');
+                $("#building").fadeOut(450, 'swing',
+                    function () {
+                        $("#alert").fadeIn(450, 'swing');
+                    });
+        }
+        // return false so any link destination is not followed
+        return false;
+    });
 
-$('a.toggleLink_sub').click(function() {
+    $('a.toggleLink_sub').click(function() {
 
-// switch visibility
-    is_visible = !is_visible;
+    // switch visibility
+        is_visible = !is_visible;
 
-// change the link text depending on whether the element is shown or hidden
-    if ($(this).text()==showText) {
-    $(this).text(hideText); $(this).parent().next('.toggle_sub').slideDown('slow');
-    }
-    else {
-    $(this).text(showText); $(this).parent().next('.toggle_sub').slideUp('slow');
-    }
+    // change the link text depending on whether the element is shown or hidden
+        if ($(this).text()==showText) {
+            $(this).text(hideText); $(this).parent().next('.toggle_sub').slideDown('slow');
+        }
+        else {
+            $(this).text(showText); $(this).parent().next('.toggle_sub').slideUp('slow');
+        }
 
-// return false so any link destination is not followed
-    return false;
+    // return false so any link destination is not followed
+        return false;
     });
 });

59df3d14b8fd670c08274cccf6dcff65c885b458
Joel Weinberger <joel@greenpangia.com>
2011-10-20 09:59:42 -0700

    Started creating table of feedback from post request

--- static/js/live_feedback_view.js
+++ static/js/live_feedback_view.js
@@ -1,9 +1,36 @@
 function live_feedback_request_building(building) {
-    $.post('view', {building: building}, function (data) {
-        var i, buildingdata;
-        buildingdata = jQuery.parseJSON(data);
+    $.post('view', {building: building}, function (us_data) {
+        var i, buildingdata, table, row;
+        var condition, priority, loc, created, other;
+        var us_floor, us_room, us_submitter, us_discomfort, us_comfort,
+            us_priority, us_comments, us_submitted;
+
+        buildingdata = jQuery.parseJSON(us_data);
+        table = $('#submissionsbody');
+        //table.empty();
         for (i in buildingdata) {
-            alert(buildingdata[i].fields.room);
+            us_floor = buildingdata[i].fields.floor;
+            us_room = buildingdata[i].fields.room;
+            us_submitter = buildingdata[i].fields.submitter;
+            us_discomfort = buildingdata[i].fields.discomfort;
+            us_comfort = buildingdata[i].fields.comfort;
+            us_priority = buildingdata[i].fields.priority;
+            us_comments = buildingdata[i].fields.comments;
+            us_submitted = buildingdata[i].fields.submitted;
+
+            // Make a table here
+            row = $('<tr class="alarm" />');
+            condition = $('<td/>');
+            if (us_discomfort && us_comfort) {
+                condition.text(us_discomfort + ',' + us_comfort);
+            } else if (us_discomfort) {
+                condition.text(us_discomfort);
+            } else {
+                condition.text(us_comfort);
+            }
+            row.append(condition);
+
+            table.append(row);
         }
     });
 }

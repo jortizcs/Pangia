a0e14a60d97e3383c1568e5a4f68a5794db8a45c
Joel Weinberger <joel@greenpangia.com>
2011-10-20 17:54:06 -0700

    generalized list in feedback

--- static/js/live_feedback_view.js
+++ static/js/live_feedback_view.js
@@ -1,4 +1,126 @@
-function live_feedback_request_building(building) {
+(function () {
+
+function pTable(columnnames, sort, reversesort)
+{
+    this.columnnames = undefined;
+    this.data = [];
+    this.numcolumns = 0;
+    this.reversesort = false;
+    this.sortindex = undefined;
+
+    // Setup column heading names
+    if (columnnames) {
+        this.columnnames = columnnames;
+        this.numcolumns = columnnames.length;
+    }
+
+    if (reversesort) {
+        this.reversesort = true;
+    }
+
+    // Setup value to sort by
+    this.sortBy(sort);
+}
+
+pTable.prototype.addRow = function (row) {
+    if (row.length !== this.numcolumns) {
+        return;
+    }
+
+    this.data.push(row);
+};
+
+pTable.prototype.sortBy = function (sort, reverse) {
+    var i;
+
+    if (sort) {
+        this.sortindex = -1;
+        for (i = 0; i < this.columnnames.length; i++) {
+            if (this.columnnames[i] === sort) {
+                this.sortindex = i;
+                break;
+            }
+        }
+    }
+
+    if (reverse) {
+        this.reversesort = true;
+    }
+
+    this.sort();
+};
+
+pTable.prototype.sort = function () {
+    var list = this;
+
+    if (this.sortindex < 0) {
+        return;
+    }
+
+    this.data.sort(function (a, b) {
+        var ret;
+
+        if (a[list.sortindex] < b[list.sortindex]) {
+            ret = -1;
+        } else if (a[list.sortindex] === b[list.sortindex]) {
+            ret = 0;
+        } else {
+            ret = 1;
+        }
+
+        if (list.reversesort) {
+            ret = ret * -1;
+        }
+
+        return ret;
+    });
+};
+
+pTable.prototype.render = function () {
+    var i, j, eltclass, table, thead, tbody, tr, th, td;
+
+    table = $('<table class="tablesorter" cellspacing="0" />');
+    thead = $('<thead />');
+    tbody = $('<tbody />');
+
+    tr = $('<tr />');
+    for (i = 0; i < this.columnnames.length; i++) {
+        th = $('<th />');
+        th.text(this.columnnames[i]);
+        tr.append(th);
+    }
+    thead.append(tr);
+    table.append(thead);
+
+    for (i = 0; i < this.data.length; i++) {
+        eltclass = '';
+        if (i % 2 == 0) {
+            eltclass = 'alarm';
+        }
+
+        tr = $('<tr class="' + eltclass + '"/>');
+
+        for (j = 0; j < this.data[i].length; j++) {
+            td = $('<td />');
+            td.text(this.data[i][j]);
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+    table.append(tbody);
+
+    return table;
+};
+
+pTable.prototype.empty = function () {
+    this.data = [];
+};
+
+var buildingList = new pTable(['Condition', 'Priority', 'Location',
+                               'Created', 'Comments'],
+                              'Priority');
+
+function request_building(building) {
     $.post('view', {building: building}, function (us_data) {
         var i, buildingdata, table, row;
         var condition, priority, loc, created, other;
@@ -6,9 +128,11 @@ function live_feedback_request_building(building) {
             us_priority, us_comments, us_submitted;
 
         buildingdata = jQuery.parseJSON(us_data);
-        table = $('#submissionsbody');
-        table.empty();
+        tablelocation = $('#submissionsbody');
+        tablelocation.empty();
+        buildingList.empty();
         for (i in buildingdata) {
+            var us_row = [];
             us_floor = buildingdata[i].fields.floor;
             us_room = buildingdata[i].fields.room;
             us_submitter = buildingdata[i].fields.submitter;
@@ -18,42 +142,22 @@ function live_feedback_request_building(building) {
             us_comments = buildingdata[i].fields.comments;
             us_submitted = buildingdata[i].fields.submitted;
 
-            // Make a table here
-            row = $('<tr class="alarm" />');
-
-            // Condition
-            condition = $('<td/>');
             if (us_discomfort && us_comfort) {
-                condition.text(us_discomfort + ', ' + us_comfort);
+                us_row.push(us_discomfort + ', ' + us_comfort);
             } else if (us_discomfort) {
-                condition.text(us_discomfort);
+                us_row.push(us_discomfort);
             } else {
-                condition.text(us_comfort);
+                us_row.push(us_comfort);
             }
-            row.append(condition);
+            us_row.push(us_priority);
+            us_row.push('Room ' + us_room + ', ' + 'Floor ' + us_floor);
+            us_row.push(us_submitted);
+            us_row.push(us_comments);
 
-            // Priority
-            priority = $('<td class="textcentered" />');
-            priority.text(us_priority);
-            row.append(priority);
-
-            // Location
-            loc = $('<td/>');
-            loc.text('Room ' + us_room + ', ' + 'Floor ' + us_floor);
-            row.append(loc);
-
-            // Created date
-            created = $('<td/>');
-            created.text(us_submitted);
-            row.append(created);
-
-            // Comments
-            other = $('<td/>');
-            other.text(us_comments);
-            row.append(other);
-
-            table.append(row);
+            buildingList.addRow(us_row);
         }
+        buildingList.sort();
+        tablelocation.append(buildingList.render());
     });
 }
 
@@ -63,7 +167,9 @@ $(document).ready(function() {
     for (i = 0; i < toggles.length; i++) {
         var elt = toggles[i];
         jQuery.data($(elt).prev()[0], 'onshow', function () {
-            live_feedback_request_building($('.buildingname', $(elt).prev())[0].textContent);
+            request_building($('.buildingname', $(elt).prev())[0].textContent);
         });
     }
 });
+
+})();

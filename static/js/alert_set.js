d7cfc66dba909ddf795870fd519e083246ba68b5
Joel Weinberger <joel@greenpangia.com>
2011-11-03 15:35:28 -0700

    added pressed buttons no alerts

--- static/js/alert_set.js
+++ static/js/alert_set.js
@@ -22,8 +22,45 @@ function list_sensors(sensorListElt) {
     sensorListElt.append(sensorList.getTable());
 }
 
+function init_buttons() {
+    var buttons = {
+        'btn_less': $('.btn_less'),
+        'btn_equal': $('.btn_equal'),
+        'btn_larger': $('.btn_larger')
+    };
+    var button;
+
+    var makeClicked  = function (buttonName) {
+        return function (event) {
+            var button = buttons[buttonName];
+            var pressedClass = buttonName + '_pressed';
+            var b, alreadypressed;
+
+            if (button.hasClass(pressedClass)) {
+                button.removeClass(pressedClass);
+            } else {
+                alreadypressed = false;
+                for (b in buttons) {
+                    if (b !== buttonName && buttons[b].hasClass(b + '_pressed')) {
+                        alreadypressed = true;
+                        break;
+                    }
+                }
+                if (!alreadypressed) {
+                    button.addClass(pressedClass);
+                }
+            }
+        };
+    };
+
+    for (button in buttons) {
+        buttons[button].click(makeClicked(button));
+    }
+}
+
 $(document).ready(function() {
     list_sensors($('#sensorlist'));
+    init_buttons();
 });
 
 })();

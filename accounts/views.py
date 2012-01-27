57b49f21fb2c79cf71590686b6a7e63326e7af8a
Joel Weinberger <joel@greenpangia.com>
2012-01-27 11:49:06 -0800

    started account management template

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def accounts_management(request):
+    return render_to_response('manage.html', context_instance=RequestContext(request, {}))

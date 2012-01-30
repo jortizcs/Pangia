cb41123bb4e70b70a3fe89d9627a08114dd3dbb5
Joel Weinberger <joel@greenpangia.com>
2012-01-30 11:33:37 -0800

    Added separate links to profile and admin management

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
 
 @login_required
-def accounts_management(request):
-    return render_to_response('manage.html', context_instance=RequestContext(request, {}))
+def accounts_profile(request):
+    u = request.user
+    return render_to_response('profile.html',
+      context_instance=RequestContext(request, {
+        "is_staff": u.is_staff
+    }))
+
+@login_required
+@staff_member_required
+def accounts_admin(request):
+    u = request.user
+    return render_to_response('admin.html',
+      context_instance=RequestContext(request, {
+        "is_staff": u.is_staff
+    }))

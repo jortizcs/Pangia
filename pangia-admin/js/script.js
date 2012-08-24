8adfff372fb9881f481194c9287e5f6eb2c3d930
Michael Heinrich <mheinrich@gmail.com>
2012-08-24 14:46:00 -0400

    Making streamfs requests more general (anything with localhost/sfs/will be matched now) + moving js scripts into a separate file

--- pangia-admin/js/script.js
+++ pangia-admin/js/script.js
@@ -1,10 +1,13 @@
 /* Author:
 
 */
-
-
-
-
+function getJSON () {
+	var url = 'http://localhost/sfs/sub/*'; //set this to whatever your localhost is running on, modify .htaccess file to make this work with node js
+	
+	 $.get(url, function(data) {
+	     alert('page content: ' + data);
+	 });
+};
 
 
 

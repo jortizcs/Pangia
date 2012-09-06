9d58f2cb8cd8860956b688ba9d7ce96fec231ebc
Michael Heinrich <mheinrich@gmail.com>
2012-09-05 23:26:44 -0400

    Adding a footer navigation element into php file (currently only functional on plotter page)

---
+++ pangia-admin/nav_footer.php
@@ -0,0 +1,13 @@
+<footer>
+      	<hr>
+      	<div class="row-fluid">
+      		<div class="well">
+		      	<?php if ($page != 'plot'): ?><div class="input-prepend input-append">
+		      		<span class="add-on">sfs://</span><input class="span11" type="text" placeholder=""><button class="btn" type="button">Go!</button>
+		      	</div>
+		      	<?php endif; ?>
+		      	<div id="msgs"><pre class="span12" style="background:white;min-height:200px; max-height:200px;overflow:auto;"></pre></div>
+		        <p>&copy; Pangia 2012</p>
+       		</div>
+   		</div>
+      </footer>

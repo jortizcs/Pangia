8e9c0355352743baae47be17c20a1f5464100f85
Michael Heinrich <mheinrich@gmail.com>
2012-09-05 23:26:15 -0400

    putting breadcrumbs navigation element into php file

--- pangia-admin/nav_breadcrumb.php
+++ pangia-admin/nav_breadcrumb.php
@@ -6,10 +6,12 @@
 		    <a href="index-instance.php">Noveda</a> <?php if (!empty($page)): ?> <span class="divider">/</span> <?php endif; ?>
 		  </li>
 		  <li class="active">
-			<?php if ($page == "files") { echo 'Files'; 
+			<?php 
+			if ($page == "files") { echo 'Files'; 
 			} elseif  ($page == "sub") { echo 'Subscriptions'; 
 			} elseif ($page == "JSON") { echo 'JSON interface'; 
 			} elseif ($page == "proc") { echo 'Processing elements'; 
-			} elseif ($page == "plot") { echo 'Plotter'; } ?>
+			} elseif ($page == "plot") { echo 'Plotter'; } 
+			?>
 		  </li> 
 		</ul>

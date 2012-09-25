2e8991e444a5f12d822385e72866f2653ce36ff0
Joel Weinberger <jww@cs.berkeley.edu>
2012-09-25 16:01:19 -0700

    Added timestamp property to regex/pubid structure to specify which grouping is the timestamp.

--- bulk-data-parser/parse.py
+++ bulk-data-parser/parse.py
@@ -1,5 +1,20 @@
 #! /usr/bin/env python
 #
+# JSON passed to parser should take form:
+#			{
+#				"map": [
+#								{
+#									"regex": <regex>,
+#									"ts":		 <index>,
+#									"pubid": <pubid>,
+#								},
+#								...
+#							 ]
+#			}
+# where <regex> is a valid Python regular expression, <pubid> is a valid
+# StreamFS pubid, and <index> is either 0 or 1, indicating which of the two
+# required group matches in "regex" is the timestamp.
+#
 # Valid regex pairs should group match first a key then a value.
 
 import sys
@@ -11,6 +26,7 @@ def getMapFromRawJSON(rawjson):
 	newobj = []
 	for i in m:
 		newobj.append({ 'regex': re.compile(i['regex']),
+						'ts': i['ts'],
 						'pubid': i['pubid']})
 
 	return newobj
@@ -19,7 +35,7 @@ def matchLine(regexmap, line):
 	for p in regexmap:
 		m = p['regex'].match(line)
 		if m and len(m.groups()) == 2:
-			return p, m.groups()[0], m.groups()[1]
+			return p, m.groups()[p['ts']], m.groups()[1 - p['ts']]
 
 	return None, None, None
 
@@ -27,15 +43,15 @@ def applyParsers(jsonmap):
 	# Generate map of regexes
 	# For each line of stdin:
 	#	For each regex:
-	#		If regex matches, publish key,value pair and break
+	#		If regex matches, publish timestamp,value pair and break
 	regexmap = getMapFromRawJSON(jsonmap)
 
 	line = sys.stdin.readline()
 	while line:
-		p,k,v = matchLine(regexmap, line)
+		p,ts,v = matchLine(regexmap, line)
 		if p:
 			# TODO publish to pubid here
-			print "key: " + k + ", value: " + v
+			print "timestamp: " + ts + ", value: " + v
 		line = sys.stdin.readline()
 
 def main():
@@ -43,7 +59,7 @@ def main():
 	Main.
 	'''
 
-	testjson = '{"map":[{"regex":"(.*),(.*)", "pubid":1}, {"regex":"(.*):(.*)","pubid":2}]}'
+	testjson = '{"map":[{"regex":"(.*),(.*)", "ts": 0, "pubid":1},{"regex":"(.*):(.*)","ts":1,"pubid":2}]}'
 	applyParsers(testjson)
 
 	return 0

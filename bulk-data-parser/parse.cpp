8fca91faeca0124ed81f7a3dac3ef131eb482a56
Joel Weinberger <jww@cs.berkeley.edu>
2012-10-30 16:38:21 -0700

    More basic c++ parser

--- bulk-data-parser/parse.cpp
+++ bulk-data-parser/parse.cpp
@@ -3,9 +3,11 @@
 
 #include <stdlib.h>
 
+#include "json_spirit/json_spirit.h"
 #include "re2/re2.h"
 
 using namespace std;
+using namespace json_spirit;
 
 string getJSONFromArg(int argc, const char* argv[]) {
     if (argc != 2) {
@@ -17,6 +19,38 @@ string getJSONFromArg(int argc, const char* argv[]) {
     return string(argv[1]);
 }
 
+bool matchLine(Object& regexmap, string& line,
+               Object& pubids, string& ts, string& v) {
+    return false;
+}
+
+void applyParsers(string& jsonmap) {
+    /*
+     * Generate map of regexes
+     * For each line of stdin:
+     *      For each regex:
+     *          If regex matches, publish timestamp,value pair and break
+     */
+
+    //string s;
+    //RE2::FullMatch("ruby:1234", "(\\w+):(.*)", &s);
+
+    //cout << s << endl;
+    Value value;
+    Object pubids;
+    string line;
+    string ts, v;
+
+    read(jsonmap, value);
+    Object regexmap = value.getObject();
+
+    getline(cin, line);
+    while(!cin.eof()) {
+        matchLine(regexmap, line, pubids, ts, v);
+
+        getline(cin, line);
+    }
+}
 
 int main(int argc, const char *argv[]) {
     string jsonstr = getJSONFromArg(argc, argv);
@@ -24,10 +58,13 @@ int main(int argc, const char *argv[]) {
     string s(argv[1]);
     cout << s << endl;
 
-    //string s;
-    //RE2::FullMatch("ruby:1234", "(\\w+):(.*)", &s);
+    //testjson = '{"map":[{"regex":"(.*),(.*)", "ts": 0, "pubid":1},{"regex":"(.*):(.*)","ts":1,"pubid":2}]}'
+    // for test-datasource.txt provided by Jorge
+    string testjson("{\"map\":[{\"regex\":\".* \\\\| ([0-9].*) \\\\| .* \\\\| (.*)\",\"ts\": 0, \"pubid\":1}]}");
+    //testjson = r'{"map":[{"regex":".* \\| ([0-9].*) \\| .* \\| (.*)","ts": 0, "pubid":1}]}'
+    jsonstr = testjson;
 
-    //cout << s << endl;
+    applyParsers(jsonstr);
 
     return 0;
 }

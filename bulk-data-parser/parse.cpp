6ee5be6aadd757edf7a8bb8ce3a2919af57a1e6a
Joel Weinberger <jww@cs.berkeley.edu>
2012-10-30 10:19:02 -0700

    Added basic C++ parser setup

--- bulk-data-parser/parse.cpp
+++ bulk-data-parser/parse.cpp
@@ -1,5 +1,33 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
-int main() {
+#include <stdlib.h>
+
+#include "re2/re2.h"
+
+using namespace std;
+
+string getJSONFromArg(int argc, const char* argv[]) {
+    if (argc != 2) {
+        cout << "You must provide exactly one argument, which is your parsing JSON object."
+             << endl;
+        exit(0);
+    }
+
+    return string(argv[1]);
+}
+
+
+int main(int argc, const char *argv[]) {
+    string jsonstr = getJSONFromArg(argc, argv);
+
+    string s(argv[1]);
+    cout << s << endl;
+
+    //string s;
+    //RE2::FullMatch("ruby:1234", "(\\w+):(.*)", &s);
+
+    //cout << s << endl;
+
     return 0;
 }

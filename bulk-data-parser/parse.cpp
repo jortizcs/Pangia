16abb9b1ceed761b7b54ea62a1d9a011ecb85db3
Joel Weinberger <jww@cs.berkeley.edu>
2012-10-30 20:35:13 -0700

    more c++ parser updates

--- bulk-data-parser/parse.cpp
+++ bulk-data-parser/parse.cpp
@@ -19,8 +19,33 @@ string getJSONFromArg(int argc, const char* argv[]) {
     return string(argv[1]);
 }
 
-bool matchLine(Object& regexmap, string& line,
-               Object& pubids, string& ts, string& v) {
+void freeREVector(vector<re2::RE2*>& regexv) {
+    for (vector<re2::RE2*>::iterator it = regexv.begin(); it != regexv.end(); it++) {
+        delete (*it);
+    }
+}
+
+void getMapFromRawJSON(string& rawjson, vector<re2::RE2*>& regexv,
+                       vector<int>& tsv, vector<int>& pubidv) {
+    Value value;
+    read(rawjson, value);
+    Array m = value.getObject()["map"].getArray();
+
+    for (vector<Value>::iterator it = m.begin(); it != m.end(); it++) {
+        Object o = (*it).getObject();
+        string regex = o["regex"].getString();
+        regexv.push_back(new RE2(regex));
+        tsv.push_back(o["ts"].getInt());
+        pubidv.push_back(o["pubid"].getInt());
+    }
+}
+
+bool matchLine(string& line,
+               vector<re2::RE2*>& regexv, vector<int>& tsv, vector<int>& pubidv,
+               int& pubid, string& ts, string& v) {
+    //cout << "matching line!" << endl;
+    for (int i = 0; i < regexv.size(); i++) {
+    }
     return false;
 }
 
@@ -35,28 +60,29 @@ void applyParsers(string& jsonmap) {
     //string s;
     //RE2::FullMatch("ruby:1234", "(\\w+):(.*)", &s);
 
-    //cout << s << endl;
-    Value value;
-    Object pubids;
-    string line;
-    string ts, v;
+    vector<re2::RE2*> regexv;
+    vector<int> tsv, pubidv;
+    getMapFromRawJSON(jsonmap, regexv, tsv, pubidv);
 
-    read(jsonmap, value);
-    Object regexmap = value.getObject();
+    int pubid;
+    string ts;
+    string value;
+    string line;
 
     getline(cin, line);
     while(!cin.eof()) {
-        matchLine(regexmap, line, pubids, ts, v);
+        matchLine(line, regexv, tsv, pubidv, pubid, ts, value);
 
         getline(cin, line);
     }
+
+    freeREVector(regexv);
 }
 
 int main(int argc, const char *argv[]) {
     string jsonstr = getJSONFromArg(argc, argv);
 
     string s(argv[1]);
-    cout << s << endl;
 
     //testjson = '{"map":[{"regex":"(.*),(.*)", "ts": 0, "pubid":1},{"regex":"(.*):(.*)","ts":1,"pubid":2}]}'
     // for test-datasource.txt provided by Jorge

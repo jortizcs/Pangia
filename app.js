7f81572bcb6fc1e9035551c3c7aa480b14a3df54
Joel Howard Willis Weinberger <joel.weinberger@gmail.com>
2013-03-31 12:21:38 -0700

    Basic test authentication scheme working. Relies on a fake user that is not stored in the DB.

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , hbs = require('express-hbs')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
-var hbs = require('express-hbs');
 
 // Hook in express-hbs and tell it where known directories reside
 app.engine('hbs', hbs.express3({
@@ -27,8 +29,10 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   // TODO look up sessions to see what to put in for 'secret'
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'mysecret' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -37,14 +41,75 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+// Basic user/password authentication
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === 'root' && password === 'root') {
+	  return done(null, TESTUSER);
+	}
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// TODO This is a fake, temporary test user that is used for testing our
+// authentication scheme. We need to add in a real DB user check at some point.
+// This test user allows you to login with username 'root' and password 'root'.
+var TESTUSER = { 'id': 1, 'username': 'root' };
+
+var findUserById = function(id, done) {
+  if (id === 1) {
+    done(null, TESTUSER);
+  } else {
+    done(new Error('User ID ' + id + ' not found.'));
+  }
+};
+
+var findUserByUsername = function(username, done) {
+  if (username === 'root') {
+    done(null, TESTUSER);
+  } else {
+    done(new Error('Username  ' + username + ' not found.'));
+  }
+};
+
+var passportCheck = function() {
+  return passport.authenticate('local', {
+    successRedirect: '/dashboard',
+	failureRedirect: '/index'
+  });
+};
+
+var ensureAuth = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/index');
+};
+
+// Currently, these are the login page. We should change this later so there is
+// an explicit login page.
 app.get('/', routes.index);
 app.get('/index', routes.index);
-app.get('/alarmshim', routes.alarmshim);
-app.get('/chart', routes.chart);
-app.get('/dashboard', routes.dashboard);
-app.post('/dashboard', routes.dashboard);
-app.get('/upload', routes.upload);
-app.get('/users', user.list);
+app.post('/index', passportCheck(), routes.index);
+
+// Make sure to *always* include a middleware 'ensureAuth' check!
+// TODO Make a function that generates GETs and POSTs that automatically
+// includes the ensureAuth middleware check.
+app.get('/alarmshim', ensureAuth, routes.alarmshim);
+app.get('/chart', ensureAuth, routes.chart);
+app.get('/dashboard', ensureAuth, routes.dashboard);
+app.post('/dashboard', ensureAuth, routes.dashboard);
+app.get('/upload', ensureAuth, routes.upload);
+app.get('/users', ensureAuth, user.list);
 
 app.post('/uploader', routes.uploader);
 

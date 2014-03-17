8d7cc73d609cd1e119257cdd83a48f9cd4708ed5
azure provisioned user <pangia@pangia1.pangia1.b5.internal.cloudapp.net>
2014-03-17 07:55:27 +0000

    small fixes

--- tools/novedaAnalysisScripts/prediction.py
+++ tools/novedaAnalysisScripts/prediction.py
@@ -54,7 +54,7 @@ def predict(dataFiles,tempPredicted,outputFile=None,figDirectory=None):
   join = join[join.mean_temp != 0]
 
   #Support Vector Regression
-  clf = svm.SVR(gamma=0.001)
+  clf = svm.SVR(gamma=0.1)
   clf.fit(zip(join.mean_temp[:-1-ignoreLastDay]), join.mean_cons[:-1-ignoreLastDay]) 
 
 
